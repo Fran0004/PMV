99cf81392b15ab21c17378246653d49af600e8b6
Fran0004 <116067683+Fran0004@users.noreply.github.com>
2024-10-25 10:16:46 +0200

    implementacion de animationTree

--- primerjuego/scripts/player.gd
+++ primerjuego/scripts/player.gd
@@ -6,6 +6,7 @@ var animacionDeSaltoUnaVez = true
 var directionParaProyectil : float = 0
 var timer
 
+@onready var anim_tree = $AnimationTree
 @onready var animated_sprite = $AnimatedSprite2D
 @onready var animated_player = $"../AnimationPlayer"
 @onready var Energy = preload("res://scens/energy_rigid_body_2d.tscn")
@@ -23,8 +24,9 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 	# Handle jump.
 	if Input.is_action_just_pressed("Saltar") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-		animacionDeSaltoUnaVez = false
+		if animacionDeSaltoUnaVez:
+			anim_tree.set("parameters/salto 2/request",AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE)
+			animacionDeSaltoUnaVez = false
 	var direction := Input.get_axis("ui_left", "ui_right")
 	
 	if direction>0:
@@ -36,13 +38,9 @@ func _physics_process(delta: float) -> void:
 		
 	if is_on_floor():
 		if direction == 0:
-			animated_sprite.play("idle")
+			anim_tree.set("parameters/Transition/transition_request","idle")
 		else:
-			animated_sprite.play("run")
-	else:
-		if animacionDeSaltoUnaVez == false:
-			animated_sprite.play("salto")
-			animacionDeSaltoUnaVez = true
+			anim_tree.set("parameters/Transition/transition_request","run")
 		
 	
 	if direction:
@@ -72,6 +70,10 @@ func _on_animated_sprite_2d_animation_finished() -> void:
 		animated_player.play("zoom_out")
 		procesing = false
 		
+func jump() ->void:
+	velocity.y = JUMP_VELOCITY
+	animacionDeSaltoUnaVez = true
+	
 func shoot(e):
 	# Accede al collider que quieres usar como punto de origen del disparo
 	if timer.is_stopped():
